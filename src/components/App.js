431d324b871e82755e20c693c0867c36f6e4d0fa
Perry Neal <perryneal11@gmail.com>
2022-08-29 20:30:17 -0400

    getting started with sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,38 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { get } from 'utils/requests';
-
-import Titlebar from 'components/titlebar/Titlebar';
-
-import styles from 'components/App.module.scss';
+import React, {  useEffect, useState } from "react";
+import { get } from "utils/requests";
+import Titlebar from "components/titlebar/Titlebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import styles from "components/App.module.scss";
+import Sidebar from "./sidebar/Sidebar";
 
 function App() {
-
-  const [response, setResponse] = useState('')
+  const [response, setResponse] = useState("");
 
   useEffect(() => {
-
     /**
      * Example call to Flask
      * @see /src/utils/requests.js
      * @see /py/crud.py
      */
-    setTimeout(() => get(
-      'api/v1/songs', // Route
-      (response) => setResponse(response), // Response callback
-      (error) => console.error(error) // Error callback
-    ), 3000);
+    setTimeout(
+      () =>
+        get(
+          "api/v1/songs", // Route
+          (response) => setResponse(response), // Response callback
+          (error) => console.error(error) // Error callback
+        ),
+      3000
+    );
   }, []);
 
   return (
-    <Fragment>
-      <Titlebar />
-
-      <div className={ styles.app }>
-        <header className={ styles['app-header'] }>
-          /api/v1/songs
-          <p>
-            {JSON.stringify(response)}
-          </p>
-          <p>
-            <a href="https://github.com/santomegonzalo/react-electron-titlebar">seems like a nice title bar</a><br/>
-            <a href="https://www.npmjs.com/package/react-pro-sidebar">sidebar idea</a><br/>
-            <a href="https://fortunar.github.io/react-sidemenu/#item3">if flexibility is needed in sidebar</a><br/>
-            <a href="https://uptick.github.io/react-keyed-file-browser/">file browser widget</a><br/>
-            <a href="https://react-ui.io/components/popover">popover can be used to display chord fingerings</a><br/>
-            <a href="https://github.com/aliustaoglu/react-js-guitar-chords#readme">get chord fingerings</a><br/>
-
-          </p>
-        </header>
+    <>
+      
+      <div className={styles.app}>
+      <Titlebar/>
+      <Sidebar />
       </div>
-    </Fragment>
+    </>
   );
 }
 
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { FaGem, FaHeart } from "react-icons/fa";
+
+function Sidebar() {
+  return (
+    <ProSidebar>
+      <Menu iconShape="square">
+        <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+        <SubMenu title="Dev Links" icon={<FaHeart />}>
+          <MenuItem>
+            <a
+              href="https://github.com/santomegonzalo/react-electron-titlebar"
+              target="_blank"
+            >
+              seems like a nice title bar
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="https://www.npmjs.com/package/react-pro-sidebar"
+              target="_blank"
+            >
+              sidebar idea
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="https://fortunar.github.io/react-sidemenu/#item3"
+              target="_blank"
+            >
+              if flexibility is needed in sidebar
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="https://uptick.github.io/react-keyed-file-browser/"
+              target="_blank"
+            >
+              file browser widget
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a href="https://react-ui.io/components/popover" target="_blank">
+              popover can be used to display chord fingerings
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="https://uptick.github.io/react-keyed-file-browser/"
+              target="_blank"
+            >
+              file browser widget
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="https://github.com/aliustaoglu/react-js-guitar-chords#readme"
+              target="_blank"
+            >
+              get chord fingerings
+            </a>
+          </MenuItem>
+        </SubMenu>
+      </Menu>
+    </ProSidebar>
+  );
+}
+
+export default Sidebar;
